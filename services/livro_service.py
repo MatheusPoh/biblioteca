3a5d453b8bdb43e47ea35268c1dc17882a2f6bb2
Matheus Pohren Oliveira <127795081+MatheusPohren@users.noreply.github.com>
2026-04-06 20:51:07 -0300

    Criação do main e da função cadastro de livros

---
+++ services/livro_service.py
@@ -0,0 +1,17 @@
+from random import choice
+
+from system.models import Livros
+livros = []
+def cadastrar_livro():
+    nome = input("Nome do livro: ").strip()
+    autor = input("Autor: ").strip()
+    if not nome or not autor:
+        print("Nome e autor são obrigatórios.")
+        return
+    for livro in livros:
+        if livro.nome.lower() == nome.lower():
+            print("Esse livro já existe!")
+            return
+    novo_livro = Livros(nome, autor)
+    livros.append(novo_livro)
+    print("livro cadastrado com sucesso!")
---
+++ system/models.py
@@ -0,0 +1,5 @@
+class Livros:
+    def __init__(self, nome, autor, disponivel=True):
+        self.nome = nome
+        self.autor = autor
+        self.disponivel = disponivel
