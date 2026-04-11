f32a78f1e0b399ce8eebc8e584eed2cc351058ea
Matheus Pohren Oliveira <127795081+MatheusPohren@users.noreply.github.com>
2026-04-11 14:09:55 -0300

    Atualizando a API

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-# CLI
-
-
-
 from system.database import criar_tabela
 from services.livro_service import cadastrar_livro, listar_livros, emprestar_livro, devolver_livro
+
 criar_tabela()
+
 def mostrar_menu():
     print("1 - Cadastrar livro")
     print("2 - Ver livros")
     print("3 - Alugar livros")
     print("4 - Devolver livro")
     print("5 - Sair")
+
 def main():
     print("Olá, digite o número do serviço que deseja")
+
     while True:
         mostrar_menu()
+
         choice = input("\n>> ").strip()
+
         if not choice.isdigit():
             print("Digite apenas números.")
             continue
@@ -32,5 +34,6 @@ def main():
             break
         else:
             print(f"\"{choice}\" não é uma opção válida")
+
 if __name__ == "__main__":
     main()
--- services/livro_service.py
+++ services/livro_service.py
@@ -1,9 +1,11 @@
-from system.models import Livros
 from system.database import inserir_livros ,listar_livros_db, alugar_livro_db, devolver_livro_db
+
 def cadastrar_livro():
     livros = listar_livros_db()
+
     nome = input("Nome do livro: ").strip()
     autor = input("Autor: ").strip()
+
     if not nome or not autor:
         print("Nome e autor são obrigatórios.")
         return
@@ -11,27 +13,39 @@ def cadastrar_livro():
         if livro.nome.lower() == nome.lower():
             print("Esse livro já existe!")
             return
+
     inserir_livros(nome, autor)
     print("livro cadastrado com sucesso!")
+
+
 def listar_livros():
     livros = listar_livros_db()
+
     if not livros:
         print("A lista está vazia!")
         return
     for livro in livros:
         print(livro)
+
+
 def emprestar_livro():
     nome = input("Qual o nome do livro que deseja alugar? ").strip().lower()
     resultado = alugar_livro_db(nome)
+
     if resultado:
         print(f"Livro \"{nome}\" emprestado com sucesso!")
     else:
         print("Livro não encontrado ou já emprestado.")
+
+
 def devolver_livro():
     nome = input("Qual livro você deseja devolver? ").strip().lower()
     resultado = devolver_livro_db(nome)
+
     if resultado:
         print(f"Livro \"{nome}\" devolvido com sucesso!")
     else:
         print("Livro não encontrado ou não alugado.")
+
+
 livros = listar_livros_db()
